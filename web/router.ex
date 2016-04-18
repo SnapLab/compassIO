d033738ccad8413aa1838d323fd666b8c0a06f02
Toby Privett <toby@snaplab.co.uk>
2016-04-18 12:02:20 +0100

    Add a route for the newly generated controller

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule CompassIO.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/caves", CaveController
   end
 
   # Other scopes may use custom stacks.
