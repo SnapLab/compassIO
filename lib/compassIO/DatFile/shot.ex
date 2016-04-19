12bee4002673f6c6e3502ea5eb95ebd4f2dd602d
Toby Privett <toby@snaplab.co.uk>
2016-04-19 16:32:40 +0100

    Calculate depth change, distance and azimuth for a shot

--- lib/compassIO/DatFile/shot.ex
+++ lib/compassIO/DatFile/shot.ex
@@ -1,4 +1,35 @@
 defmodule CompassIO.DatFile.Shot do
   defstruct from_station: "", to_station: "", length: "", bearing: "",
     inclination: "", flags: "", comment: ""
+
+  def depth_change(shot) do
+    {degrees, _} = Float.parse(shot.inclination)
+
+    (:math.sin(to_radians(degrees)) * distance(shot))
+    |> Float.round(0)
+    |> positivity(shot)
+  end
+
+  def distance(shot) do
+    {val, _} = Float.parse(shot.length)
+    val
+  end
+
+  def azimuth(shot) do
+    {val, _} = Integer.parse(shot.bearing)
+    val
+  end
+
+  defp down?(shot) do
+    Float.parse(shot.inclination) < 0
+  end
+
+  defp to_radians(degrees) do
+    degrees * (:math.pi / 180)
+  end
+
+  defp positivity(number, shot) do
+    if down?(shot), do: (0 - number), else: number
+  end
 end
+
