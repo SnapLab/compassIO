3458229e19dfb523aac787dae61afc61fc1626ba
Toby Privett <toby@snaplab.co.uk>
2016-05-16 13:23:34 +0100

    Correct ordering  of surveys and stations
    - the moral of the story is:
    Ecto query gives more control than tacky preloads

--- web/controllers/map_controller.ex
+++ web/controllers/map_controller.ex
@@ -2,14 +2,17 @@ defmodule CompassIO.MapController do
   use CompassIO.Web, :controller
 
   alias CompassIO.Cave
+  alias CompassIO.Survey
+  alias CompassIO.Station
   alias CompassIO.StationBuilder
 
   def show(conn, %{"id" => id}) do
     StationBuilder.build(Repo.get!(Cave, id))
-    cave =
-      Repo.get!(Cave, id)
-      |> Repo.preload(surveys: [stations: (from s in CompassIO.Station, order_by: s.id)])
+    cave = Repo.get!(Cave, id)
+    surveys =
+      Repo.all(from s in Survey, where: s.cave_id == ^cave.id, order_by: s.id)
+      |> Repo.preload(stations: from(s in Station, order_by: s.id))
 
-    render(conn, "show.html", cave: cave)
+    render(conn, "show.html", cave: cave, surveys: surveys)
   end
 end
