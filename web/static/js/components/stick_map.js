df9e77a24a87da70b2067790f47ad58dbe80a1f3
Toby Privett <toby@snaplab.co.uk>
2016-07-04 16:35:30 +0100

    rendering the paths as  group

--- web/static/js/components/stick_map.js
+++ web/static/js/components/stick_map.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import Paper from "paper";
+import paper from "paper";
 import { fetchCave } from '../actions/index';
 
 export default class StickMap extends React.Component {
@@ -8,32 +8,37 @@ export default class StickMap extends React.Component {
     this.props.fetchCave();
   }
 
-  // componentDidMount() {
-  //   paper.setup('canvas');
-  //   var path = new paper.Path();
-  //   // Give the stroke a color
-  //   path.strokeColor = 'red';
-  //   var start = new paper.Point(100, 100);
-  //   // Move to start and draw a line from there
-  //   path.moveTo(start);
-  //   // Note that the plus operator on Point objects does not work
-  //   // in JavaScript. Instead, we need to call the add() function:
-  //   path.lineTo(start.add([ 200, -50 ]));
-  //   // Draw the view now:
-  //   paper.view.draw();
+  componentDidMount() {
+    paper.setup('canvas');
 
-  //   }
+    var group = new paper.Group
+    var segments = [new paper.Point(0,0), new paper.Point(100, 100), new paper.Point(200, 100), new paper.Point(300, 100)];
 
+    var path = new paper.Path(segments);
+
+    path.strokeColor = 'red';
+    group.addChild(path)
+    // group.rotate(-90)
+
+    var inner_canvas = new paper.Path.Rectangle({
+      point: [50, 50],
+      size: [400, 400]
+    });
+    group.fitBounds(inner_canvas.bounds)
+    paper.view.draw();
+
+  };
 
   render() {
-    const cave= this.props.cave;
+    const cave = this.props.cave;
+
     if (!cave) {
       return <div>Loading...</div>
     }
     return (
       <div>
-      <h1>{cave.name}</h1>
-      <canvas id="canvas" width={600} height={600}/>
+        <h2>{cave.name}</h2>
+        <canvas id="canvas" width={500} height={500} style={{backgroundColor: 'black'}} />
       </div>
     );
   }
