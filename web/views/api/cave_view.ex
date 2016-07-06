c2de05a2b9c435047734bcad9ed62f66388943eb
Toby Privett <toby@snaplab.co.uk>
2016-07-06 16:59:06 +0100

    Render a rough SVG

--- web/views/api/cave_view.ex
+++ web/views/api/cave_view.ex
@@ -11,7 +11,8 @@ defmodule CompassIO.Api.CaveView do
         %{
             id: cave.id,
             name: cave.name,
-            surveys: Enum.map(surveys, &render_survey(&1, stations))
+            surveys: Enum.map(surveys, &render_survey(&1, stations)),
+            svg: svg(surveys, stations)
         }
      }
   end
@@ -30,6 +31,41 @@ defmodule CompassIO.Api.CaveView do
        point: Geo.JSON.encode(station.point),
        entrance_distance: station.entrance_distance
     }
+  end
+
+  def svg(surveys, stations) do
+    "<svg>
+      <g transform=\"translate(450,250) rotate(-90)\">
+      #{ svg_polylines(surveys, stations) }
+      <g>
+    </svg>"
+  end
+
+  def svg_polylines(surveys, stations) do
+    surveys
+    |> Enum.map(&svg_polyline(&1, stations))
+  end
+
+  def svg_polyline(survey, stations) do
+    "<polyline points=\"#{
+      Enum.join(svg_points(survey, stations), " ")
+    }\" />"
+  end
+
+  def svg_points(survey, stations) do
+    survey.shots
+    |> Enum.map(&svg_point(&1.station_to, stations))
+    |> List.insert_at(0, svg_point(survey.tie_in, stations))
+  end
+
+  def svg_point(station, stations) do
+    if String.strip(station) == "" do
+      "0,0"
+    else
+      stations[String.to_atom(station)].point.coordinates
+      |> Tuple.to_list
+      |> Enum.join(",")
+    end
   end
 
   def render_survey(survey, stations) do
