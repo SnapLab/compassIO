5b26206705e2c6e1b87e56f5ee5ef0ebc4506bc3
Toby Privett <toby@snaplab.co.uk>
2016-07-12 11:05:54 +0100

    Rotating the coordinates around the axis

--- web/services/svg_builder.ex
+++ web/services/svg_builder.ex
@@ -55,7 +55,7 @@ defmodule CompassIO.SvgBuilder do
   # given a stations, return the svg friendly coordinates
   defp svg_coordinates(station_name, cave, stations) do
     coordinates_at(station_name, stations)
-    |> coordinate_transfomer(cave.svg_canvas_x, cave.svg_canvas_y, stations)
+    |> coordinate_transfomer(cave, stations)
     |> Tuple.to_list
     |> Enum.join(",")
   end
@@ -65,24 +65,28 @@ defmodule CompassIO.SvgBuilder do
     station_atoms[String.to_atom(station_name)].point.coordinates
   end
 
-  defp coordinate_transfomer({cart_x,cart_y}, canvas_x, canvas_y, stations, target_width \\ 400) do
+  defp coordinate_transfomer({cart_x, cart_y},
+      %Cave{svg_canvas_x: canvas_x, svg_canvas_y: canvas_y},
+      stations, target_width \\ 400) do
+
+    scale_factor = target_width / Enum.max([canvas_x, canvas_y])
 
     # 0. get the min x and min y coords
     coords = Enum.map(stations, &(Tuple.to_list(&1.point.coordinates)))
     x_min = Enum.min(Enum.map(coords, &(List.first(&1))))
     y_min = Enum.min(Enum.map(coords, &(List.last(&1))))
 
-    # 1. shift the coordinates to the new axis
-    screen_x = cart_x - x_min
-    screen_y = cart_y - y_min
+    # 1. shift the coordinates to the new axis and scale
+    shifted_x = (cart_x - x_min) * scale_factor
+    shifted_y = ((cart_y - y_min) * scale_factor)
 
-    # 2. flip the map
-    # screen_x = (x_max/2 + (cart_x)*1)
-    # screen_y = (y_max/2 - (cart_y)*-1)
+    # 2. rotate to display 'north' up
+    # http://benn.org/2007/01/06/rotating-coordinates-around-a-centre/
+    center = target_width/2
+    angle = Maths.to_radians(-90)
+    screen_x =  center +  :math.cos(angle) * (shifted_x - center) -  :math.sin(angle) * (shifted_y - center)
+    screen_y = center + :math.sin(angle) * (shifted_x - center) +  :math.cos(angle) * (shifted_y - center)
 
-    # 3. scale for fit the tartet
-    scale_factor = target_width / Enum.max([canvas_x, canvas_y])
-    {screen_x*scale_factor,screen_y*scale_factor}
-    # WIP: http://stackoverflow.com/questions/14880601/translating-between-cartesian-and-screen-coordinates
+    {screen_x,screen_y}
   end
 end
